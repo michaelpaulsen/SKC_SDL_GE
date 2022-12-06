1790d9b29656dfacf188b6524c145f7d232915b1
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-05 21:54:32 -0800

    added EventQue class
    this stores all of the game events and has a public facing API that allows you to call the handles based on a given name.
    it currently runs though all of the current events and does them if the name is matching.
    adding multiple events of the same name is yields undefined behavior, as I can not guarantee that it will not return after the first call.  [also this is not something that

--- ConsoleApplication2/EventHandler.h
+++ ConsoleApplication2/EventHandler.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 #include <functional>
 #include <SDL.h>
 struct EventHandler
@@ -18,5 +19,29 @@ struct EventHandler
 	}
 	void operator()(SDL_Event t) {
 		this->callback(t);
+	}
+};
+
+class EventQue {
+	std::vector<EventHandler> events;
+public:
+	void registerEvent(EventHandler hndl) {
+		events.push_back(hndl);
+		//this should not be used as it copyies the handle instead of making one in place. 
+	}
+	void registerEvent(std::string n, EventHandler::CallBack_T hndl) {
+		events.emplace_back(n, hndl);
+		//this constructs an handel in place this is the form of this function that should be used the most 
+	}
+	void registerEvent(EventHandler::CallBack_T hndl) {
+		events.emplace_back(hndl);
+		//this constructs an handel in place this is the form of this function that should be used the most 
+	}
+	void callEventByName(std::string name, SDL_Event t) {
+		for (auto& i : this->events) {
+			if (i.name == name) {
+				i(t);
+			}
+		}
 	}
 };
