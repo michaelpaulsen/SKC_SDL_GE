329caf18d5953ce9ffca27ab8bd9d69d68382c88
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-05 22:14:17 -0800

    added a TID (type id) field to the EventHandler struct
    [v-a0.1.0]
    I also added the TID to the constructor. The TID should always be the SDL_EventType that the event should fire on if it is not the
    Event will not fire because the event loop will call based off of this value.
    added a callEventByTID this is the same as callEventByName exept that it comares the tid instead of the name.
    marked the callEventByName as deprecated.

--- ConsoleApplication2/EventHandler.h
+++ ConsoleApplication2/EventHandler.h
@@ -6,16 +6,18 @@
 struct EventHandler
 {
 	typedef std::function<void(SDL_Event)> CallBack_T;
-
+	Uint32 EventTid;
 	CallBack_T callback;
 	std::string name;
-	EventHandler(std::string n, CallBack_T callbackfunction) {
+	EventHandler(std::string n, Uint32 TID, CallBack_T callbackfunction) {
 		callback = callbackfunction;
 		name = n;
+		EventTid = TID; 
 	}
 	explicit EventHandler(CallBack_T callbackfunction) {
 		callback = callbackfunction;
 		name = "default";
+		EventTid = 0; 
 	}
 	void operator()(SDL_Event t) {
 		this->callback(t);
@@ -29,17 +31,25 @@ public:
 		events.push_back(hndl);
 		//this should not be used as it copyies the handle instead of making one in place. 
 	}
-	void registerEvent(std::string n, EventHandler::CallBack_T hndl) {
-		events.emplace_back(n, hndl);
+	void registerEvent(std::string n, Uint32 TID, EventHandler::CallBack_T hndl) {
+		events.emplace_back(n, hndl, TID);
 		//this constructs an handel in place this is the form of this function that should be used the most 
 	}
 	void registerEvent(EventHandler::CallBack_T hndl) {
 		events.emplace_back(hndl);
 		//this constructs an handel in place this is the form of this function that should be used the most 
 	}
+	[[deprecated]] 
 	void callEventByName(std::string name, SDL_Event t) {
 		for (auto& i : this->events) {
 			if (i.name == name) {
+				i(t);
+			}
+		}
+	}
+	void callEventByTID(Uint32 tid,  SDL_Event t) {
+		for (auto& i : this->events) {
+			if (i.EventTid == tid) {
 				i(t);
 			}
 		}
