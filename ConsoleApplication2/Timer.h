b40fbd53acc9e03a05f00d11271c2ec58f21510f
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-14 14:09:38 -0800

    [v a 0.5.1]  made Wait return the difference of the two timepoints

--- ConsoleApplication2/Timer.h
+++ ConsoleApplication2/Timer.h
@@ -8,13 +8,11 @@ namespace Skele_lib {
 			typedef std::chrono::time_point<std::chrono::system_clock> chrono_sysclock_t;
 			typedef std::chrono::milliseconds chrono_ms_t;
 			typedef long double ElapsedTime_T; 
-			void Wait(chrono_sysclock_t tp1, chrono_sysclock_t tp2, chrono_ms_t frameTime, ElapsedTime_T & fElapsedTime) {
+			auto Wait(const chrono_sysclock_t &tp1, const chrono_sysclock_t &tp2, chrono_ms_t frameTime, ElapsedTime_T & fElapsedTime) {
 				std::chrono::duration<float> difference = tp2 - tp1;
-				tp1 = tp2;
 				std::this_thread::sleep_for(frameTime - difference);
 				fElapsedTime = (long double)frameTime.count() / 1000;
-				//this is safe because we know that a g_frameCount always takes frameTime ms
-
+				return difference;
 			}
 		}
 	}
