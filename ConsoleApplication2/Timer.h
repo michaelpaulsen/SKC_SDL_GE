84f1e89fb4c80b9a6c53acd74c434fa87b3799f3
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-14 15:12:43 -0800

    [v a 0.5.2] added frameCount_t
    frameCount_t is a typedef of unsigned long long. this will be used in a few places.

--- ConsoleApplication2/Timer.h
+++ ConsoleApplication2/Timer.h
@@ -4,9 +4,9 @@
 namespace Skele_lib {
 	namespace SKGE {
 		namespace Timer {
-
 			typedef std::chrono::time_point<std::chrono::system_clock> chrono_sysclock_t;
 			typedef std::chrono::milliseconds chrono_ms_t;
+			typedef unsigned long long frameCount_t;
 			typedef long double ElapsedTime_T; 
 			auto Wait(const chrono_sysclock_t &tp1, const chrono_sysclock_t &tp2, chrono_ms_t frameTime, ElapsedTime_T & fElapsedTime) {
 				std::chrono::duration<float> difference = tp2 - tp1;
