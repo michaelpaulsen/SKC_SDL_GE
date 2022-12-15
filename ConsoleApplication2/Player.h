b27d2de5d7e30961f9df0fe0eafe326c59ff2a5f
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-15 12:51:28 -0800

    Update Player.h

--- ConsoleApplication2/Player.h
+++ ConsoleApplication2/Player.h
@@ -4,19 +4,19 @@
 namespace Skele_lib {
 	namespace SKGE {
 		struct Player{
-			Sprite sprite;
-			vec2d position; 
-			vec2d size; 
-			int	score;
+			Sprite m_sprite;
+			vec2d m_position; 
+			vec2d m_size; 
+			int	m_score;
 			Player(const char* path, SDL_Renderer* renderer, vec2d _s, vec2d _p = { 0,0 }) {
 				vec2d rect = { _s.x,_s.y };
-				sprite = Sprite(path, renderer, rect);
-				position = { 0,0 }; 
-				size = _s;
-				score = 0; 
+				m_sprite = Sprite(path, renderer, rect);
+				m_position = { 0,0 }; 
+				m_size = _s;
+				m_score = 0; 
 			}
 			SDL_Rect getPlayerRect() {
-				return { (int)position.x,(int)position.y, (int)size.x, (int)size.y };	
+				return {(int)m_position.x,(int)m_position.y,(int)m_size.x,(int)m_size.y};	
 			}
 		};
 	}
