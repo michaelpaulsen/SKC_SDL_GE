1406ed0cf60bcbaefd4ed1469d13597107f9d43f
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-14 21:46:09 -0800

    [v - a 0.6.8] gave Sprite ownership of its own UV.
    the function that sets the uv map doesn't take a pointer so you can construct in place for non-comlicated uses

--- ConsoleApplication2/sprite.h
+++ ConsoleApplication2/sprite.h
@@ -7,12 +7,15 @@ namespace Skele_lib {
 		class Sprite {
 			SDL_Texture* m_texture = NULL;
 			vec2d m_size;
+			SDL_Rect* UVMap; 
 		public:
 			Sprite() {
-				m_size = { 0,0 }; 
+				m_size = { 0,0 };
+				UVMap = NULL; 
 			}
 			Sprite(const char* path, SDL_Renderer* renderer, vec2d size = {0,0}) {
 				SDL_Surface* loadedSurface = SDL_LoadBMP(path);
+				UVMap = NULL; 
 				if (loadedSurface != NULL)
 				{
 					m_texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
@@ -20,8 +23,21 @@ namespace Skele_lib {
 				}
 				m_size = size;
 			}
-			void DrawSprite( SDL_Renderer* renderer, SDL_Rect* srcpos, SDL_Rect* destpos) {
-				SDL_RenderCopy(renderer, this->m_texture, srcpos, destpos);
+			void DrawSprite( SDL_Renderer* renderer, SDL_Rect* destpos) {
+				SDL_RenderCopy(renderer, this->m_texture, UVMap, destpos);
+			}
+			void SetUVMap(SDL_Rect uvm) {
+				if (!UVMap) {
+					UVMap = (SDL_Rect*)malloc(sizeof(SDL_Rect)); 
+				}
+				UVMap->x = uvm.x; 
+				UVMap->y = uvm.y; 
+				UVMap->w = uvm.w; 
+				UVMap->h = uvm.h; 
+			}
+			void ClearUVMap() {
+				free(UVMap);
+				UVMap = NULL;
 			}
 		};
 	}
