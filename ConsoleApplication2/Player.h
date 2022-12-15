5b37dbf65777c5c3fff3d5eb5c483fcef47b0622
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-15 12:57:59 -0800

    [v - a 0.6.18] made "overrides"  for all of the sprite functions
    this are not actually overrides because Sprite is a child of Player not a parent to it, but what I did was made it so that you don't have to go Player::sprite::function you can just go Player::function.

--- ConsoleApplication2/Player.h
+++ ConsoleApplication2/Player.h
@@ -6,17 +6,31 @@ namespace Skele_lib {
 		struct Player{
 			Sprite m_sprite;
 			vec2d m_position; 
-			vec2d m_size; 
+			vec2d m_size, m_baseSize;
 			int	m_score;
 			Player(const char* path, SDL_Renderer* renderer, vec2d _s, vec2d _p = { 0,0 }) {
 				vec2d rect = { _s.x,_s.y };
 				m_sprite = Sprite(path, renderer, rect);
 				m_position = { 0,0 }; 
-				m_size = _s;
+				m_size =m_baseSize = _s;
 				m_score = 0; 
 			}
 			SDL_Rect getPlayerRect() {
 				return {(int)m_position.x,(int)m_position.y,(int)m_size.x,(int)m_size.y};	
+			}
+			void SetUVMap(SDL_Rect uvm) {
+				m_sprite.SetUVMap(uvm);
+			}
+			void ClearUVMap() {
+				m_sprite.ClearUVMap();
+			}
+			void SetScale(double scale) {
+				m_size *= scale;
+				m_sprite.SetScale(scale); 
+			}
+			void ClearScale() {
+				m_size = m_baseSize; //this is why we have m_baseSize 
+				m_sprite.ClearScale();
 			}
 		};
 	}
