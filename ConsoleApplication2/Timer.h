88847b0101ccba3dfe3777cc92cc9d816524343f
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-14 18:59:02 -0800

    Update Timer.h

--- ConsoleApplication2/Timer.h
+++ ConsoleApplication2/Timer.h
@@ -7,7 +7,7 @@ namespace Skele_lib {
 			typedef std::chrono::time_point<std::chrono::system_clock> chrono_sysclock_t;
 			typedef std::chrono::milliseconds chrono_ms_t;
 			typedef unsigned long long frameCount_t;
-			typedef long double ElapsedTime_T; 
+			typedef long double ElapsedTime_t; 
 			auto Wait(const chrono_sysclock_t &tp1, const chrono_sysclock_t &tp2, chrono_ms_t frameTime, ElapsedTime_T & fElapsedTime) {
 				std::chrono::duration<float> difference = tp2 - tp1;
 				std::this_thread::sleep_for(frameTime - difference);
