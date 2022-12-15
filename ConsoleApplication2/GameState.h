63496141bcc0ff5d67480116ae35f5981adfc86d
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-14 21:56:34 -0800

    [v - a 0.6.10] added  std::vector<Player> playerQue
    playerQue is a vector of players that are in the game world.
    also added a way to emplace onto the back of the vector
    and a way to get a player from the vector

--- ConsoleApplication2/GameState.h
+++ ConsoleApplication2/GameState.h
@@ -1,20 +1,31 @@
 #pragma once
 #include "./EventHandler.h"
+#include <vector>
 #include <SDL.h>
+#include "./Player.h"
 namespace Skele_lib {
 	namespace SKGE {
-		struct World {
+		struct World{
 			unsigned long long frame;
 			long double fElapsedTime;
 			int TFPS; 
-			
+			std::vector<Player> playerQue; 
+			size_t players;
 			World(int tfs) {
 				TFPS = tfs; 
 				frame = 0;
-				fElapsedTime = 0; 
+				fElapsedTime = 0;
+				players = 0; 
 			}
 			std::chrono::milliseconds GetFrameTime() {
 				return std::chrono::milliseconds(1000 / TFPS);
+			}
+			void AddPlayer(const char* path, SDL_Renderer* renderer, vec2d _s, vec2d _p = { 0,0 }) {
+				playerQue.emplace_back(path, renderer, _s, _p);
+				players++; 
+			}
+			Player& GetPlayerAt(size_t index) {
+				return playerQue.at(index);
 			}
 		};
 	}
