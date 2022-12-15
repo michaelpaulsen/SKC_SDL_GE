baa59606a2d672ca4a3eb5095898afbc8a9eac9e
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-14 18:58:08 -0800

    [v - a 0.6.5] removed m_lastPos & m_lastUV from Sprite Class
    removed m_lastPos & m_lastUV from Sprite as they are will not be used

--- ConsoleApplication2/sprite.h
+++ ConsoleApplication2/sprite.h
@@ -6,29 +6,22 @@ namespace Skele_lib {
 	namespace SKGE {
 		class Sprite {
 			SDL_Texture* m_texture = NULL;
-			SDL_Rect m_lastPos, m_lastUV;
 			vec2d m_size;
 		public:
-			Sprite(const char* path, SDL_Renderer* renderer) {
+			Sprite() {
 				m_size = { 0,0 }; 
+			}
+			Sprite(const char* path, SDL_Renderer* renderer, vec2d size = {0,0}) {
 				SDL_Surface* loadedSurface = SDL_LoadBMP(path);
 				if (loadedSurface != NULL)
 				{
 					m_texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
 					SDL_FreeSurface(loadedSurface);
 				}
-				m_lastPos = { 0,0,0,0 };
-				m_lastUV = { 0,0,0,0 };
 				m_size = size;
 			}
 			void DrawSprite( SDL_Renderer* renderer, SDL_Rect* srcpos, SDL_Rect* destpos) {
 				SDL_RenderCopy(renderer, this->m_texture, srcpos, destpos);
-				if (srcpos) {
-					m_lastUV = *srcpos;
-				}
-				if (destpos) {
-					m_lastPos = *destpos;
-				}
 			}
 		};
 	}
