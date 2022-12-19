0f465bb40a062083c777d9d850317914fb61af67
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-18 16:24:07 -0800

    [v - a 8.9.1]

--- ConsoleApplication2/headers/Physics.h
+++ ConsoleApplication2/headers/Physics.h
@@ -11,7 +11,7 @@ namespace Skele_lib {
 				m_force.m_y = -m_force.m_y;
 			}
 			void ApplyForceAndSetPos(Vector::Vec2d& position, Vector::Vec2d& force, Vector::Vec2d drag ) {
-				if (force.m_x) { position.m_x += force.m_x; if (force.m_x > 0) { force.m_x -= drag.m_x;} if (force.m_x < 0) { force.m_x += drag.m_x; }}
+				if (force.m_x) {position.m_x += force.m_x; if (force.m_x > 0) { force.m_x -= drag.m_x;} if (force.m_x < 0) { force.m_x += drag.m_x; }}
 				if (force.m_y) {position.m_y += force.m_y; if (force.m_y > 0) { force.m_y -= drag.m_y; } if (force.m_y < 0) { force.m_y += drag.m_y; }}
 			}
 			void AddForce(Vector::Vec2d& m_force, Vector::Vec2d force) {
