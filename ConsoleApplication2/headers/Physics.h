d176609425f919dd00e4fb99ab374d2be384d03b
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-18 16:49:48 -0800

    [v - a 8.10.0]
    changed the ApplyForceAndSetPos so that if the force is less than the drag force it sets the force to zero instead of using the drag to bring it closer to zero thus flipping but making it still less than the |drag| ( given that |y| < |x| |x-y| must be less than |x|)

--- ConsoleApplication2/headers/Physics.h
+++ ConsoleApplication2/headers/Physics.h
@@ -11,8 +11,29 @@ namespace Skele_lib {
 				m_force.m_y = -m_force.m_y;
 			}
 			void ApplyForceAndSetPos(Vector::Vec2d& position, Vector::Vec2d& force, Vector::Vec2d drag ) {
-				if (force.m_x) {position.m_x += force.m_x; if (force.m_x > 0) { force.m_x -= drag.m_x;} if (force.m_x < 0) { force.m_x += drag.m_x; }}
-				if (force.m_y) {position.m_y += force.m_y; if (force.m_y > 0) { force.m_y -= drag.m_y; } if (force.m_y < 0) { force.m_y += drag.m_y; }}
+				if (force.m_x) {
+					position.m_x += force.m_x;
+					if (force.m_x > drag.m_x) {
+						force.m_x -= drag.m_x;
+					}else if (force.m_x < -drag.m_x) {
+						force.m_x += drag.m_x;
+					}
+					else {
+						force.m_x = 0; 
+					}
+				}	
+				//maybe imlement this as a function to dry the code? 
+				if (force.m_y) {
+					position.m_y += force.m_y;
+					if (force.m_y > drag.m_y) {
+						force.m_y -= drag.m_y;
+					}else if (force.m_y < -drag.m_y) {
+						force.m_y += drag.m_y;
+					}
+					else {
+						force.m_y = 0; 
+					}
+				}
 			}
 			void AddForce(Vector::Vec2d& m_force, Vector::Vec2d force) {
 				m_force += force;
