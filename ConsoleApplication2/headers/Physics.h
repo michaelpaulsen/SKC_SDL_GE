42ea7da4c31422c32c681f508a64d3cb8d61d769
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-19 18:14:17 -0800

    [v - a 8.11.1] moved drag force calculation to its own function

--- ConsoleApplication2/headers/Physics.h
+++ ConsoleApplication2/headers/Physics.h
@@ -10,29 +10,26 @@ namespace Skele_lib {
 			void BounceVirtical(Vector::Vec2d& m_force) {
 				m_force.m_y = -m_force.m_y;
 			}
+			void ApplyForce(double& force, double drag) {
+				if (force > drag) {
+					force -= drag;
+				}
+				else if (force < -drag) {
+					force += drag;
+				}
+				else {
+					force = 0;
+				}
+			}
 			void ApplyForceAndSetPos(Vector::Vec2d& position, Vector::Vec2d& force, Vector::Vec2d drag ) {
 				if (force.m_x) {
 					position.m_x += force.m_x;
-					if (force.m_x > drag.m_x) {
-						force.m_x -= drag.m_x;
-					}else if (force.m_x < -drag.m_x) {
-						force.m_x += drag.m_x;
-					}
-					else {
-						force.m_x = 0; 
-					}
+					ApplyForce(force.m_x, drag.m_x);
 				}	
 				//maybe imlement this as a function to dry the code? 
 				if (force.m_y) {
 					position.m_y += force.m_y;
-					if (force.m_y > drag.m_y) {
-						force.m_y -= drag.m_y;
-					}else if (force.m_y < -drag.m_y) {
-						force.m_y += drag.m_y;
-					}
-					else {
-						force.m_y = 0; 
-					}
+					ApplyForce(force.m_y, drag.m_y);
 				}
 			}
 			void AddForce(Vector::Vec2d& m_force, Vector::Vec2d force) {
