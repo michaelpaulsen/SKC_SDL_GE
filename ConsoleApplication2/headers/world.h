4b0e81b84fb0770f6b198d7b6ce9362f153dafa8
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-19 20:51:30 -0800

    [v - a 8.14.2] added GetPlayerCount to World and sorted the functions.

--- ConsoleApplication2/headers/world.h
+++ ConsoleApplication2/headers/world.h
@@ -7,6 +7,7 @@
 #include "./Window.h"
 #include "./Physics.h"
 #include "./sprite.h"
+#include "./EventHandler.h"
 #ifndef MAX_PLAYERS
 #define MAX_PLAYERS 4 
 #endif // !MAX_PLAYERS
@@ -30,19 +31,21 @@ namespace Skele_lib {
 			std::chrono::milliseconds GetFrameTime() {
 				return std::chrono::milliseconds(1000 / TFPS);
 			}
+			Player& GetPlayerAt(size_t index) {
+				if (index > m_players) {
+					return  l_players[m_players];
+				}
+				return l_players[index];
+
+			}
+			FrameN_t GetFrame() {return frame;}
+			size_t GetPlayerCount() { return m_players;}
 			void AddPlayer(const char* path, SDL_Renderer* renderer, Vector::Vec2d _s, Vector::Vec2d _p, Vector::Vec2d _d) {
 				if (m_players != MAX_PLAYERS) {
 					l_players[m_players] = Player(path, renderer, _s, _p,_d);
 					m_players++;
 
 				}
-			}
-			Player& GetPlayerAt(size_t index) {
-				if (index > m_players) {
-					return  l_players[m_players]; 
-				}
-				return l_players[index]; 
-
 			}
 			void DrawPlayers(SDL_Renderer* r) {
 				for (size_t i = 0; i < m_players; i++){
@@ -71,15 +74,13 @@ namespace Skele_lib {
 					}
 				}
 			}
-			FrameN_t GetFrame() {
-				return frame;
-			}
 			void IncFrame() {
 				++frame;
 			}
 			void SetTargetFPS(FrameN_t targetFPS) {
 				TFPS = targetFPS;
 			}
+
 		};
 	}
 }
