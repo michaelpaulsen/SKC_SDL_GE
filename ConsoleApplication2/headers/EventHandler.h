5bcb497e9f5fa44f76004f9b676c047b723a43f2
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-20 11:06:42 -0800

    [v - a 8.16.0] added ColideWithWalls to EventHandler.h
    this triggers an event when ever the player collides with a wall.

--- ConsoleApplication2/headers/EventHandler.h
+++ ConsoleApplication2/headers/EventHandler.h
@@ -65,5 +65,16 @@ namespace Skele_lib {
 				}
 			}
 		};
+		void ColideWithWalls(int w, int h, EventQue& eque, World wrld) {
+			for (size_t i = 0; i < wrld.GetPlayerCount(); i++) {
+				auto& player = wrld.GetPlayerAt(i);
+				auto& force = player.GetForce();
+				auto rect = player.getPlayerRect();
+				if (rect.x <= 0 && force.m_x < 0) { eque.callEventByName("P_hit_left_bound",NULL, wrld, i);}
+				if (rect.x >= w - rect.w && force.m_x > 0) { eque.callEventByName("P_hit_right_bound", NULL, wrld, i); }
+				if (rect.y <= 0 && force.m_y < 0) { eque.callEventByName("P_hit_top_bound", NULL, wrld, i);}
+				if (rect.y >= h - rect.h && force.m_y > 0) { eque.callEventByName("P_hit_bottom_bound", NULL, wrld, i); }
+			}
+		}
 	}
 }
