9cad214922dd16fc2e25ea6f2a6668b632e4400a
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-20 11:45:32 -0800

    [v - a 8.16.0] changed to using Event_t in EventHandler.h
    this effects
    registerEvent and callEventByTID

--- ConsoleApplication2/headers/EventHandler.h
+++ ConsoleApplication2/headers/EventHandler.h
@@ -4,23 +4,24 @@
 #include <functional>
 #include <SDL.h>
 #include "./world.h"
+#include "Event_t.h"
 namespace Skele_lib {
 	namespace SKGE {
 		struct EventHandler
 		{
 			typedef std::function<void(const SDL_Event*, World&, size_t pid)> CallBack_T;
-			Uint32 EventTid;
+			Event_t eventTid; 
 			CallBack_T callback;
 			std::string name;
-			EventHandler(std::string n, Uint32 TID, CallBack_T callbackfunction) {
+			EventHandler(std::string n, Event_t TID, CallBack_T callbackfunction) {
 				callback = callbackfunction;
 				name = n;
-				EventTid = TID;
+				eventTid = TID;
 			}
 			explicit EventHandler(CallBack_T callbackfunction) {
 				callback = callbackfunction;
 				name = "default";
-				EventTid = 0;
+				eventTid = { 0, false };
 			}
 			void operator()(const SDL_Event* t, World w, size_t pid) {
 				this->callback(t, w,pid);
@@ -34,16 +35,16 @@ namespace Skele_lib {
 				events.push_back(hndl);
 				//this should not be used as it copyies the handle instead of making one in place. 
 			}
-			void registerEvent(std::string n, Uint32 TID, EventHandler::CallBack_T hndl) {events.emplace_back(n, TID, hndl);}
+			void registerEvent(std::string n, Event_t TID, EventHandler::CallBack_T hndl) {events.emplace_back(n, TID, hndl);}
 			[[depercated("use registerEvent(std::string n, Uint32 TID, EventHandler::CallBack_T hndl) instead")]]
 			void registerEvent(EventHandler::CallBack_T hndl) {
 				events.emplace_back(hndl);
 				//this constructs an handel in place this is the form of this function that should be used the most 
 			}
-			void callEventByTID(Uint32 tid, const SDL_Event* t, World world, size_t pid = -1) {
+			void callEventByTID(Event_t tid, const SDL_Event* t, World world, size_t pid = -1) {
 				bool found = false; // at least one callback was called 
 				for (auto& i : this->events) {
-					if (i.EventTid == tid) {
+					if (i.eventTid == tid) {
 						found = true;
 						i(t, world, pid);
 					}
@@ -57,7 +58,7 @@ namespace Skele_lib {
 				for (auto& i : this->events) {
 					if (i.name == name) {
 						found = true;
-						i.callback(t, world);
+						i.callback(t, world, pid);
 					}
 				}
 				if (!found) {
--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -16,12 +16,12 @@ int windw = 1920, windh = 1080;
 size_t wFlags = SDL_WINDOW_SHOWN, rFlags = SDL_RENDERER_ACCELERATED;
 int main(int argc, char* args[]){
 	auto window = Skele_lib::SKGE::Window("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windw, windh,wFlags,rFlags);
-	world.AddPlayer("C:/Users/Skele/source/repos/SKC_SDL_GE/ConsoleApplication2/img/testSprite.bmp", window.renderer, { 64, 64 }, { (double)windw/2, 0 }, { 0.025, 0.025 });
-	world.AddPlayer("C:/Users/Skele/source/repos/SKC_SDL_GE/ConsoleApplication2/img/testSprite.bmp", window.renderer, { 64, 64 }, { (double)(windw / 2)+64, 0 }, { 0.5, 0.5 });
+	world.AddPlayer("C:/Users/Skele/source/repos/SKC_SDL_GE/ConsoleApplication2/img/testSprite.bmp", window.renderer, { 64, 64 }, { 00, 00 }, { 00.025, 00.025 });
+	world.AddPlayer("C:/Users/Skele/source/repos/SKC_SDL_GE/ConsoleApplication2/img/testSprite.bmp", window.renderer, { 64, 64 }, { 64, 00 }, { 00.500, 00.500 });
 	world.GetPlayerAt(0).SetUVMap({ 0,0,16,16 });
 	world.GetPlayerAt(1).SetUVMap({ 16,0,16,16 });
-	eQue.registerEvent("SDL_KEYDOWN",            SDL_KEYDOWN,            [](SDL_Event e, Skele_lib::SKGE::World &world) {
-		auto sym = e.key.keysym.sym;
+	eQue.registerEvent("SDL_KEYDOWN", { SDL_KEYDOWN, true }, [](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
+		auto sym = e->key.keysym.sym;
 		auto& player = world.GetPlayerAt(0);
 		auto& spring = world.GetPlayerAt(1); 
 		switch (sym) {
@@ -47,18 +47,22 @@ int main(int argc, char* args[]){
 
 			}
 			});
-	eQue.registerEvent("SDL_MOUSEMOTION",        SDL_MOUSEMOTION,        [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_WINDOWEVENT",        SDL_WINDOWEVENT,        [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_KEYDOWN",            SDL_KEYUP,              [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_TEXTEDITING",        SDL_TEXTEDITING,        [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_KEYMAPCHANGED",      SDL_KEYMAPCHANGED,      [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_AUDIODEVICEADDED",   SDL_AUDIODEVICEADDED,   [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_AUDIODEVICEREMOVED", SDL_AUDIODEVICEREMOVED, [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_MOUSEBUTTONDOWN",    SDL_MOUSEBUTTONDOWN,    [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_MOUSEBUTTONUP",      SDL_MOUSEBUTTONUP,      [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_MOUSEWHEEL",         SDL_MOUSEWHEEL,         [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_TEXTINPUT",          SDL_TEXTINPUT,          [](SDL_Event e, Skele_lib::SKGE::World &world) {});
-	eQue.registerEvent("SDL_KEYMAPCHANGED",      SDL_KEYMAPCHANGED,      [](SDL_Event e, Skele_lib::SKGE::World &world) {});
+	eQue.registerEvent("SDL_MOUSEMOTION",		        { SDL_MOUSEMOTION, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_WINDOWEVENT",               { SDL_WINDOWEVENT, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_KEYDOWN",                         { SDL_KEYUP, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_TEXTEDITING",               { SDL_TEXTEDITING, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_KEYMAPCHANGED",           { SDL_KEYMAPCHANGED, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_AUDIODEVICEADDED",     { SDL_AUDIODEVICEADDED, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_AUDIODEVICEREMOVED", { SDL_AUDIODEVICEREMOVED, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_MOUSEBUTTONDOWN",        { SDL_MOUSEBUTTONDOWN,true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_MOUSEBUTTONUP",            { SDL_MOUSEBUTTONUP,true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_MOUSEWHEEL",                  { SDL_MOUSEWHEEL,true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_TEXTINPUT",			          { SDL_TEXTINPUT, true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("SDL_KEYMAPCHANGED",            { SDL_KEYMAPCHANGED,true}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) {});
+	eQue.registerEvent("P_hit_top_bound",                            { 0, false}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) { printf("player %llu hit the         top of the world\n",pid+1); });
+	eQue.registerEvent("P_hit_bottom_bound",                         { 0, false}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) { printf("player %llu hit the      bottom of the world\n",pid+1); });
+	eQue.registerEvent("P_hit_left_bound",                           { 0, false}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) { printf("player %llu hit the left  bound of the world\n",pid+1); });
+	eQue.registerEvent("P_hit_right_bound",                          { 0, false}, [](const SDL_Event* e, Skele_lib::SKGE::World &world,size_t pid) { printf("player %llu hit the right bound of the world\n",pid+1); });
 	auto sdlinitstate = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 	if (sdlinitstate  < 0){
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -74,7 +78,7 @@ int main(int argc, char* args[]){
 	while (!quit) {
 		while (SDL_PollEvent(&e)) {
 			if(e.type != SDL_QUIT){	
-				eQue.callEventByTID(e.type, e, world);
+				eQue.callEventByTID({ e.type,true } , &e, world);
 				continue;
 			}
 			quit = true; 
@@ -83,10 +87,12 @@ int main(int argc, char* args[]){
 		auto& spring = world.GetPlayerAt(1);
 		Skele_lib::SKGE::Physics::CaculateAndAddSpringForce(spring.GetPosition(), player.GetPosition(), spring.GetForce(), 0.0025, 64);
 		world.ApplyForceToPlayers();
+		Skele_lib::SKGE::ColideWithWalls(windw, windh, eQue, world);
+
 		world.BouncePlayersOffWorldBounds(windw, windh);
+		
 		///=== DRAW START ===
 		window.SetDrawColor(255, 255, 255);		
-	
 		window.ClearScreenToDrawColor();
 
 		///=== DRAW THINGS START ===
