2c0a634b5e7bcd5c5a9e2e012f539267c82438f7
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-28 00:47:55 -0800

    [v - a 8.18.0] changed Vec2d to Vec<size_t size, typename T>
    this greatly simplifyies the code as I don't need to make copies of this file for other types. this allowed me to remove Vec2i.h hence the huge delta.

--- ConsoleApplication2/headers/Vec2d.h
+++ ConsoleApplication2/headers/Vec2d.h
@@ -4,138 +4,146 @@
 namespace Skele_lib {
 	namespace SKGE {
 		namespace Vector {
-			struct Vec2d {
+			template <typename T> T tabs(T number) {
+				if (number < 0) {
+					return -number; 
+				}
+				return number; 
+			}
+			template <typename T> struct Vec2 {
 				union {
-					double m_x, m_w;
+					T m_x, m_w;
 				};
 				union {
-					double m_y, m_h;
+					T m_y, m_h;
 				};
-
-				Vec2d(double x, double y) {
+				Vec2(T x, T y) {
 					m_x = x;
 					m_y = y;
 				}
-				Vec2d() {
+				Vec2() {
 					m_x = 0;
 					m_y = 0;
 				}
-				bool IsXLess(Vec2d o)  { return m_x < o.m_x;}
-				bool IsYLess(Vec2d o)  { return m_y < o.m_y;}
-				bool IsXMore(Vec2d o)  { return m_x > o.m_x;}
-				bool IsYMore(Vec2d o)  { return m_y > o.m_y;}
-				bool IsXLess(double o) { return m_x < o;}
-				bool IsYLess(double o) { return m_y < o;}
-				bool IsXMore(double o) { return m_x > o;}
-				bool IsYMore(double o) { return m_y > o;}
+				bool IsXLess(Vec2 o)  { return m_x < o.m_x;}
+				bool IsYLess(Vec2 o)  { return m_y < o.m_y;}
+				bool IsXMore(Vec2 o)  { return m_x > o.m_x;}
+				bool IsYMore(Vec2 o)  { return m_y > o.m_y;}
+				bool IsXLess(T o) { return m_x < o;}
+				bool IsYLess(T o) { return m_y < o;}
+				bool IsXMore(T o) { return m_x > o;}
+				bool IsYMore(T o) { return m_y > o;}
 				SDL_Rect ToSDL_Rect() { return { 0,0,(int)round(m_w),(int)round(m_h) };}
 				SDL_Rect AsOriginOfSDL_Rect(int width, int height) { return { (int)round(m_x),(int)round(m_y), width, height }; }
-				SDL_Rect AsOriginOfSDL_Rect(Vec2d o) { return { (int)round(m_x),(int)round(m_y),(int)round(o.m_x),(int)round(o.m_y) };}
-				Vec2d AbsDist(Vec2d o) {return { fabs(m_x - o.m_x), fabs(m_y - o.m_y) };}
-				Vec2d Dist(Vec2d o) {return { (m_x - o.m_x), (m_y - o.m_y) };}
-				static Vec2d PosFromSDL_Rect(SDL_Rect rect) {return { (double)rect.x, (double)rect.y };}
+				SDL_Rect AsOriginOfSDL_Rect(Vec2 o) { return { (int)round(m_x),(int)round(m_y),(int)round(o.m_x),(int)round(o.m_y) };}
+				Vec2 AbsDist( Vec2 o) { return { tabs(m_x - o.m_x), tabs(m_y - o.m_y) };}
+				Vec2 Dist   ( Vec2 o) { return {     (m_x - o.m_x),     (m_y - o.m_y) };}
+				static Vec2 PosFromSDL_Rect(SDL_Rect rect) {return { (T)rect.x, (T)rect.y};}
 				double Fleng() {return m_x * m_x + m_y * m_y;}	
-				Vec2d operator*(Vec2d o) {
-					Vec2d temp;
+				Vec2 operator*(Vec2 o) {
+					Vec2 temp;
 					temp.m_x = m_x * o.m_x;
 					temp.m_y = m_y * o.m_y;
 					return temp;
 				}
-				Vec2d operator/(Vec2d o) {
-					Vec2d temp;
+				Vec2 operator/(Vec2 o) {
+					Vec2 temp;
 					temp.m_x = m_x / o.m_x;
 					temp.m_y = m_y / o.m_y;
 					return temp;
 				}
-				Vec2d operator+(Vec2d o) {
-					Vec2d temp;
+				Vec2 operator+(Vec2 o) {
+					Vec2 temp;
 					temp.m_x = m_x + o.m_x;
 					temp.m_y = m_y + o.m_y;
 					return temp;
 				}
-				Vec2d operator-(Vec2d o) {
-					Vec2d temp;
+				Vec2 operator-(Vec2 o) {
+					Vec2 temp;
 					temp.m_x = m_x - o.m_x;
 					temp.m_y = m_y - o.m_y;
 					return temp;
 				}
-				Vec2d operator-(double o) {
+				Vec2 operator-(T o) {
 					//this is a short hand for this - {o,o}; 
-					Vec2d temp;
+					Vec2 temp;
 					temp.m_x = m_x - o;
 					temp.m_y = m_y - o;
 					return temp;
 				}
-				Vec2d operator+(double o) {
+				Vec2 operator+(T o) {
 					//this is a short hand for this - {o,o}; 
-					Vec2d temp;
+					Vec2 temp;
 					temp.m_x = m_x + o;
 					temp.m_y = m_y + o;
 					return temp;
 				}
-				Vec2d operator/(double i) {
-					Vec2d temp;
+				Vec2 operator/(T i) {
+					Vec2 temp;
 					temp.m_x = m_x / i;
 					temp.m_y = m_y / i;
 					return temp;
 				}
-				Vec2d operator*(double i) {
-					Vec2d temp;
+				Vec2 operator*(T i) {
+					Vec2 temp;
 					temp.m_x = m_x * i;
 					temp.m_y = m_y * i;
 					return temp;
 				}
-				void operator*=(Vec2d o) {
+				void operator*=(Vec2 o) {
 					m_x *= o.m_x;
 					m_y *= o.m_y;
 				}
-				void operator*=(double i) {
+				void operator*=(T i) {
 					m_x *= i;
 					m_y *= i;
 				}
-				void operator/=(Vec2d o) {
+				void operator/=(Vec2 o) {
 					m_x  /= o.m_x;
 					m_y  /= o.m_y;
 				}
-				void operator/=(double i) {
+				void operator/=(T i) {
 					m_x /= i;
 					m_y /= i;
 				}
-				void operator-=(Vec2d o) {
+				void operator-=(Vec2 o) {
 					m_x -= o.m_x;
 					m_y -= o.m_y;
 				}
-				void operator+=(Vec2d o) {
+				void operator+=(Vec2 o) {
 					m_x += o.m_x;
 					m_y += o.m_y;
 				}						  
-				void operator-=(double o) {
+				void operator-=(T o) {
 					// this is not technically a valid operation in LA but this is a short hand for
 					// this - [o,o] wich is so that is why I implement it. 
-
 					m_x -= o;
 					m_y -= o;
 				}
-				void operator+=(double o) {
+				void operator+=(T o) {
 					// this is not technically a valid operation in LA but this is a short hand for
 					// this + [o,o] wich is so that is why I implement it. 
-
 					m_x += o;
 					m_y += o;
 				}
-				void operator=(Vec2d o) {
+				void operator=(Vec2 o) {
 					m_x = o.m_x;
 					m_y = o.m_y;
 				}
-				void operator=(double o) {
+				void operator=(T o) {
 					m_x = o;
 					m_y = o;
 				}
-				bool operator==(Vec2d o) { return (m_x == o.m_x) && (m_y == o.m_x);  }
-				bool operator!=(Vec2d o) { return !(*this == o);}
-				bool operator<(Vec2d o)  { return (m_x < o.m_x) && (m_y < o.m_y); }
-				bool operator>(Vec2d o)  { return (m_x > o.m_x) && (m_y > o.m_y); }
+				bool operator==(Vec2 o) { return (m_x == o.m_x) && (m_y == o.m_x);  }
+				bool operator!=(Vec2 o) { return !(*this == o);}
+				bool operator<(Vec2 o)  { return (m_x < o.m_x) && (m_y < o.m_y); }
+				bool operator>(Vec2 o)  { return (m_x > o.m_x) && (m_y > o.m_y); }
 			};
+			typedef Vec2<double> Vec2d;
+			typedef Vec2<int> Vec2i;
+			typedef Vec2<float> Vec2f;
+			typedef Vec2<long> Vec2l;
+			typedef Vec2<unsigned long> Vec2u;
 		}
 	}
 }
