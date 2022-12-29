6519373b14f43dbc8b63944495244c42d4fdb3a2
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-12-28 18:28:35 -0800

    [v - a 8.19] created LOGGER class
    this is for basic logging to the console. this class will replace printf going forward it is recommended that it be used as it allows you to track what is printing what by default.

---
+++ ConsoleApplication2/headers/LOGGER.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <cstdarg> 
+
+#ifndef c_str 
+#define c_str char* 
+#endif // !c_str 
+
+namespace Skele_lib {
+	namespace SKGE {
+		class LOGGER {
+			c_str m_name;
+		public:
+			LOGGER(const c_str name) {
+				m_name = (c_str)malloc(strlen(name)+1);
+				size_t i = 0; 
+				while (name[i]) {
+					m_name[i] = name[i]; 
+					++i;
+				}
+				m_name[i++] = 0; 
+			}
+			size_t Log(const c_str fmt, ...) const {
+				size_t leng = 0; 
+				leng += printf("[%s] ", m_name); 
+				va_list args;
+				va_start(args, fmt);
+				leng += vprintf(fmt, args); 
+				va_end(args);
+				return leng;
+			}
+			size_t Print(const c_str fmt, ...) {
+				size_t leng = 0;
+				va_list args;
+				va_start(args, fmt);
+				leng += vprintf(fmt, args);
+				va_end(args);
+				return leng;
+			}
+			size_t PrintTo( const int endpos, const int startpos = 0) {
+				size_t pos = startpos, retval = 0; 
+				while (pos < endpos) {
+					auto incval = printf(" "); 
+					retval += incval; 
+					pos += incval; 
+				}
+				return retval; 
+			}
+		};
+	}
+}
