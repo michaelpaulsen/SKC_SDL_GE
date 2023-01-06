e06f7d708dd9f454a40512f0874513f51683476a
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-01-05 21:54:13 -0800

    [v - a 9.1.0] added enum class SDL_KeyModifiers
    this is for used when checking if any modifiers were pressed when a SDL_KEDOWN event took place.

---
+++ ConsoleApplication2/headers/KeyModifiers.h
@@ -0,0 +1,18 @@
+#pragma once
+namespace Skele_lib {
+	namespace SKGE {
+		enum class SDL_KeyModifiers
+		{
+			RIGHT_SHIFT = 1,
+			LEFT_SHIFT  = 2,
+			LEFT_CTRL   = 64,
+			RIGHT_CTRL  = 128,
+			LEFT_ALT    = 256,
+			RIGHT_ALT   = 512,
+			SHIFT = 3,
+			CTRL = 192,
+			ALT = 768
+		};
+	}
+}
+
