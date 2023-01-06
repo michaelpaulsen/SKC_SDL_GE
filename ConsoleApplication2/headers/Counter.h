22e0dca4d4c6253e5a430f9553f189cc0d398769
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-01-05 17:05:47 -0800

    [v - a 8.22.0] created Counter
    this is a counter that you can set the length of and the max length of. when the internal count is less than the length it will trigger a TimerActiveTrigger with the passed in pid as the counter's uid, when the counter is reset (when count is == m_max) it triggers a TimerTriggerReset event with the pid set to the counters uid.
    in the example this is used to draw some triangles to the screen when the timer is active an a circle when the timer resets.

---
+++ ConsoleApplication2/headers/Counter.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "./EventHandler.h"
+namespace Skele_lib {
+	namespace SKGE {
+		class Counter {
+			size_t m_count = 0, m_max = 0xff, m_leng = 1, m_uid;
+			 static size_t nutid;
+		public:
+
+			Counter() {
+				m_uid = nutid++;
+			}
+			void IncCount() {
+				++m_count;
+			}
+			void setLeng(size_t nleng) {
+				m_leng = nleng;
+			}
+			void setMax(size_t nleng) {
+				m_max = nleng;
+			}
+			bool IsActive() {
+				return m_count <= m_leng;
+			}
+			void operator()(World w, EventQue eq) {
+				if (m_count < m_leng) {
+					eq.callEventByName("TimerActiveTrigger", NULL,  w, m_uid);
+				}
+				if (m_count == m_max) {
+					eq.callEventByName("TimerTriggerReset", NULL, w, m_uid);
+					m_count = 0;
+				}else{
+					++m_count;
+				}
+			}
+		};
+		size_t Counter::nutid = 0;
+	}
+}
