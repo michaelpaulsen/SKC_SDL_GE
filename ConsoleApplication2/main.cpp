6409460a069d1d467f67a1f9b2cb88f6be704b36
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-01-21 19:00:03 -0800

    [v - a 10 example]

--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -1,48 +1,36 @@
 #define skc_W64
 #include <SDL.h>
-#include <stdio.h>
-#include <thread>
 #include <chrono>
 #include <random>
-#include "headers/EventHandler.h"
-#include "headers/KeyModifiers.h"
-#include     "headers/funtions.h"
-#include      "headers/Physics.h"
-#include      "headers/Counter.h"
-#include      "headers/Defines.h"
-#include       "headers/Player.h"
-#include       "headers/Window.h"
-#include       "headers/LOGGER.h"
-#include        "headers/Level.h"
-#include        "headers/Timer.h"
-#include        "headers/world.h"
-#include          "headers/RNG.h"
-#define CBuffer(name, size) char name[size] 
-#define clearBuffer(s, size, buffer) for(size_t s = 0; s < size; s++){buffer[s] = 0;}
+#include "headers/Counter.h"
+#include "headers/Defines.h"
+#include  "headers/LOGGER.h"
+#include   "headers/Level.h"
+#include   "headers/Timer.h"
+#include   "headers/world.h"
+#include     "headers/RNG.h"
 int main(int argc, char* args[]) {
+	auto level = Skele_lib::SKGE::Formats::Level("C:\\Users\\Skele\\source\\repos\\SKC_SDL_GE\\assets\\level.lvl");
+	int mode = 0;
 	///=== creating world ===  
 	const int targetFPS = 60; 
 	auto world = Skele_lib::SKGE::World(targetFPS);
 	///=== creating frame limiting stuff ===
 	Skele_lib::SKGE::Timer::chrono_sysclock_t tp1, tp2;
 	Skele_lib::SKGE::EventQue eQue;
-	
 	///=== creating logging ===
 	auto mdl = Skele_lib::SKGE::LOGGER("MAIN - DBUG");
 	auto mel = Skele_lib::SKGE::LOGGER("MAIN - ERROR");
 	auto mwl = Skele_lib::SKGE::LOGGER("MAIN - WARN");
 	auto mil = Skele_lib::SKGE::LOGGER("MAIN - INFO");
-
 	///=== creating window ===
 	auto sdlinitstate = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 	if (sdlinitstate < 0) {
 		mel.Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 		return sdlinitstate;
 	}
-
 	///=== points ===
-	SDL_Point points[3000];
-	
+	SDL_Point points[3000] = { 0 };
 	///=== create Event Timers ===
 	auto triangleTimer = Skele_lib::SKGE::Counter();
 	auto rectcounter = Skele_lib::SKGE::Counter();
@@ -51,11 +39,9 @@ int main(int argc, char* args[]) {
 	rectcounter.setLeng(10); 
 	rectcounter.setMax(50); 
 	///=== creating window ===
-	int windw = 1920, windh = 1080;
-	uint32_t wFlags = SDL_WINDOW_SHOWN, rFlags = SDL_RENDERER_ACCELERATED;
+	int windw = 1080, windh = 1080;
+	uint32_t wFlags = SDL_WINDOW_SHOWN |SDL_WINDOW_RESIZABLE, rFlags = SDL_RENDERER_ACCELERATED;
 	auto window = Skele_lib::SKGE::Window("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windw, windh, wFlags, rFlags);
-
-	
 	///=== Set up the random number generator===
 	int minr = 0, maxr = 100;
 	int NUM_POINTS = 100;
@@ -63,30 +49,9 @@ int main(int argc, char* args[]) {
 	/// draw the frame
 	bool drawFrame = true;
 	bool showinv = false;
+	bool minimized = false; 
 	///=== registering the keydown event===
-	eQue.registerEvent("SDL_KEYDOWN",        { SDL_KEYDOWN, true }, [mil, mwl, mdl, mel,&window, &drawFrame, &showinv](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
-		auto sym = e->key.keysym.sym;
-		printf("SDL_KEDOWN EVENT PROC %c                               \n", sym);
-		window.SetPuase(true);
-
-		switch (sym) {
-			case 'q': {
-				drawFrame = !drawFrame; 
-				break; 
-			}
-			case 'e': {
-				showinv = !showinv; 
-				break; 
-			}
-			default: {
-				
-				mil.Log("unhandled key %c press (0x%x)                    \n", sym, sym); 
-				return;
-			}
-
-		}
-	});
-	eQue.registerEvent("TimerActiveTrigger", { 0, false }, [mil, mwl, mdl, mel, &window](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
+	eQue.registerEvent("TimerActiveTrigger",       { 0,                        false }, [mil, mwl, mdl, mel, &window, &showinv, &mode,  &eQue, &rectcounter](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
 		switch (pid) {
 			case 0: {
 				window.RenderDrawTriangle(100, 100, 150, 150, 100, 150);
@@ -99,11 +64,40 @@ int main(int argc, char* args[]) {
 			}
 		}
 	});
-	eQue.registerEvent("TimerTriggerReset", { 0, false }, [mil, mwl, mdl, mel, &window, &rectcounter, &eQue](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
+	eQue.registerEvent("TimerTriggerReset",        { 0,                        false }, [mil, mwl, mdl, mel, &window, &showinv, &mode,  &eQue, &rectcounter](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
 		if (pid == 0) {
 			window.RenderFilledCircle(16, 16, 16); 
 			rectcounter(world, eQue); 
 		}
+	});
+	eQue.registerEvent("SDL_KEYDOWN",              { SDL_KEYDOWN,              true  }, [mil, mwl, mdl, mel, &window, &showinv, &mode,  &eQue, &rectcounter](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
+		auto sym = e->key.keysym.sym;
+	switch (sym) {
+	case 'q': {
+		window.TogglePause();
+		coutf("{:<100}\n", window.IsPaused() ? "window is paused" : "window is not paused");
+		break;
+	}
+	case 'e': {
+		showinv = !showinv;
+		window.ToggleInvintory(0, true);
+		break;
+	}
+	case 'm': {
+		(++mode) %= 2;
+	}
+	default: {
+		return;
+	}
+
+	}
+	});
+	eQue.registerEvent("SDL_RENDER_TARGETS_RESET", { SDL_RENDER_TARGETS_RESET, true  }, [mil, mwl, mdl, mel, &window, &windh,   &windw, &minimized](const SDL_Event* e, Skele_lib::SKGE::World& world, size_t pid) {
+		SDL_GetWindowSize(window.window, &windw, &windh); 
+		window.windowSize = { windw, windh };
+		auto type = e->window.event; 
+		if (type == SDL_WINDOWEVENT_MINIMIZED) minimized = true; 
+		if (type == SDL_WINDOWEVENT_RESTORED || type == SDL_WINDOWEVENT_MAXIMIZED) minimized = false; 
 	});
 	///=== set up the random things that I don't have a better owner for===
 	SDL_Event e;
@@ -121,53 +115,50 @@ int main(int argc, char* args[]) {
 			}
 			quit = true;
 		}
-
 		///=== DRAW START ===
-		
 		if (drawFrame) {
-			window.SetDrawColor(0, 0, 0);
+			auto size = Skele_lib::SKGE::Vector::Vec2d();
+			size.m_w = ((double)(windw/1) / (double)level.GetSize().m_w);
+			size.m_h = ((double)(windh/1) / (double)level.GetSize().m_h);
+			window.SetDrawColor(0xff, 0xff, 0xff);
 			window.ClearScreenToDrawColor();
-			window.SetDrawColor(255, 255, 0);
-			if (!showinv) {
-				for (size_t i = 0; i < NUM_POINTS; i++) {
-					auto randomNumber = rng.GetNextNumberAsPercent(minr, maxr);
-					float ty = ((float)windh * (((randomNumber * 2) - 1) / 2));
-					int px = (int)(i * ((double)windw / NUM_POINTS));
-					int py = ty + (windh / 2);
-					points[i] = { px,py };
+			for (size_t y = 0; y < level.GetSize().m_y; y++) {
+				for (size_t x = 0; x < level.GetSize().m_y; x++) {
+					int xpos = x * size.m_w;
+					int ypos = y * size.m_h;
+					switch (level.At(x, y)) {
+						case 0: {
+							window.SetDrawColor(100, 255, 100);
+							break; 
+						}
+						case 1: {
+							window.SetDrawColor(0, 0, 0);
+							break; 
+						}
+						default: {
+							window.SetDrawColor(255, 255, 255);
+						}
+					}
+					window.RenderFillRect(xpos, ypos, size.m_w+1, size.m_h+1);
 				}
 			}
-			window.RenderDrawLines(points, NUM_POINTS);
-			if (showinv) {
-				window.DrawInventory(9,5);
-			}
-			if (SDL_GetError()[0] != 0) {
-				mel.Log("Frame %llu -> SDL_ERROR  %s", world.GetFrame(), SDL_GetError()); 
-				exit(-1); 
-			}
-			triangleTimer(world,eQue);
 			window.UpdateScreen();
-			
 		}
-		world.DrawPlayers(window.renderer); 
+		drawFrame = !(window.IsPaused() || minimized);
 		///===================================
 		///=== DRAW THINGS           START ===
-		/// 
+		///world.DrawPlayers(window.renderer);
 		///=== DRAW THINGS             END ===
 		///=== DRAW                    END ===
 		///=== Increment the frame counter ===
 		///===================================
-		clearBuffer(u, 200, titlebuffer);
+		auto titlestr = std::format("{} - {} fps drawing {} inv {}", "ENTER GAME TITLE HERE", 1.0 / world.dElapsedTime, drawFrame, showinv); 
 #ifdef _DEBUG
-		
-		sprintf_s(titlebuffer, "%s - %f fps [DEBUG] drawing %d showing invintory %d ","ENTER GAME TITLE HERE", 1.0 / world.dElapsedTime, drawFrame, showinv);
+		titlestr += " [DEBUG]"; 
+#endif // _DEBUG
 
-#elif
-		sprintf_s(titlebuffer, "%s %s", "ENTER GAME TITLE HERE",  1.0 / world.dElapsedTime, drawFrame ? "" : "[paused]");
-#endif // DEBUG
-		window.SetWindowTitle(titlebuffer);
+		window.SetWindowTitle(titlestr.c_str());
 		world.IncFrame();
-		
 		///=== Frame Limit Start === 
 		tp2 = std::chrono::system_clock::now();
 		Skele_lib::SKGE::Timer::Wait(tp1, tp2, world.GetFrameTime(), world.dElapsedTime);
