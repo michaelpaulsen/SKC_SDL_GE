332c558724b1a36a498b9fe33ce8c34d6d714d68
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-01-22 14:27:19 -0800

    [v - a 11.0.0] changed window to class (from struct)

--- ConsoleApplication2/headers/Window.h
+++ ConsoleApplication2/headers/Window.h
@@ -3,9 +3,10 @@
 #include "./vec3d.h"
 #include "Vec2d.h"
 #include "Defines.h"
+
 namespace Skele_lib {
 	namespace SKGE {
-		struct Window {
+		class Window {
             typedef unsigned char ucolor_t; 
 			SDL_Window* window = NULL;
             std::string windowTitle; 
@@ -15,52 +16,60 @@ namespace Skele_lib {
             bool m_isPaused = false, m_isInInvintory = false;
             size_t m_activeInvintory;
             char lastAlpha;
-            size_t GetActiveInvinory()                 { return m_activeInvintory;           }
-            bool IsPaused()                            { return        m_isPaused;           }
-            bool IsInInvintory()                       { return   m_isInInvintory;           }
-            void SetPuase(bool p)                      { m_isPaused = p;                     }
-            void TogglePause()                         { m_isPaused = m_isPaused?false:true; }
-            void SetWindowTitle(const std::string str) { SetWindowTitle(str.c_str());        }
+        public :
             Window(std::string title, int x, int y, int w, int h, Uint32 windowFlags, Uint32 rendererFlags) {
-                windowTitle = title; 
+                windowTitle = title;
                 window = SDL_CreateWindow(title.c_str(), x, y, w, h, windowFlags);
-				renderer = SDL_CreateRenderer(window, -1, rendererFlags);
-				windowSize = {w,h}; 
-				lastAlpha = 0; 
-			}
-			~Window() {
-				SDL_DestroyRenderer(renderer);
-				SDL_DestroyWindow(window); 
-			}
-            void SetWindowTitle(const char* title) {
+                renderer = SDL_CreateRenderer(window, -1, rendererFlags);
+                windowSize = { w,h };
+                lastAlpha = 0;
+            }
+            ~Window() {
+                SDL_DestroyRenderer(renderer);
+                SDL_DestroyWindow(window);
+            }
+
+            SDL_Window* const GetWindow() const                     { return        window;               }
+            Vector::Vec2i     GetWindowSize() const                 { return        windowSize;           }
+            size_t            GetActiveInvinory() const             { return m_activeInvintory;           }
+            bool              IsPaused() const                      { return        m_isPaused;           }
+            bool              IsInInvintory() const                 { return   m_isInInvintory;           }
+            void              SetPuase(bool p)                      { m_isPaused = p;                     }
+            void              TogglePause()                         { m_isPaused = m_isPaused?false:true; }
+            void              SetWindowTitle(const std::string str) { SetWindowTitle(str.c_str());        }
+            void              SetWindowTitle(const char* title) {
                 windowTitle = title;
                 SDL_SetWindowTitle(window, title);
             }
-            void ToggleInvintory(size_t pid = 0, bool puseGame = true) {
+            void              ToggleInvintory(size_t pid = 0, bool puseGame = true) {
                 m_activeInvintory = pid;
                 m_isInInvintory = !m_isInInvintory;
             }
-            void ClearRenderer() {
+            void              ClearRenderer() {
 				SDL_RenderClear(renderer);
 			}
-			void UpdateScreen() {
+			void              UpdateScreen() {
 				SDL_RenderPresent(renderer); 
 			}
-			void SetDrawColor(ucolor_t r, ucolor_t g, ucolor_t b, ucolor_t a = 255) {
+			void              SetDrawColor(ucolor_t r, ucolor_t g, ucolor_t b, ucolor_t a = 255) {
 				SDL_SetRenderDrawColor(renderer, r, g, b, a);
 				lastColor.r = r; 
 				lastColor.g = g; 
 				lastColor.b = b;
 				lastAlpha   = a;
 			}
-			SDL_Rect WindowSizeToSdlRect() {
+            void              SetSize(Vector::Vec2i &wind) {     
+                SDL_GetWindowSize(window, &wind.m_w, &wind.m_h);
+                windowSize = { wind.m_w, wind.m_h };
+            }
+            SDL_Rect          WindowSizeToSdlRect() {
 				return { 0,0, (int)windowSize.m_x,(int)windowSize.m_y };
 			}
-			void ClearScreenToDrawColor() {
+			void              ClearScreenToDrawColor() {
 				auto r = WindowSizeToSdlRect(); 
 				SDL_RenderFillRect(renderer, &r);
 			}
-            void RenderFilledCircle( double tx, double ty, double r) {
+            void              RenderFilledCircle( double tx, double ty, double r) {
                 double topbound = ty - r;
                 double bottombound = ty + r;
                 double leftbound = tx - r;
@@ -75,7 +84,7 @@ namespace Skele_lib {
                     }
                 }
             }
-            void  RenderDrawCircle(int x, int y, int radius) {
+            void              RenderDrawCircle(int x, int y, int radius) {
                 int offsetx, offsety, d;
                 int status;
                 offsetx = 0;
@@ -110,23 +119,23 @@ namespace Skele_lib {
                     }
                 }
             }
-            void RenderFillRect(int x, int y, int w, int h) {
+            void              RenderFillRect(int x, int y, int w, int h) {
                 SDL_Rect rect = { x,y,w,h }; 
                 SDL_RenderFillRect(renderer, &rect);
             }
-            void RenderDrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
+            void              RenderDrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
                 SDL_RenderDrawLine(renderer, x1, y1, x2, y2); 
                 SDL_RenderDrawLine(renderer, x2, y2, x3, y3); 
                 SDL_RenderDrawLine(renderer, x1, y1, x3, y3); 
             }
-            void RenderDrawLines(SDL_Point* pos, const size_t count) {
+            void              RenderDrawLines(SDL_Point* pos, const size_t count) {
                 SDL_RenderDrawLines(renderer, pos, count);
             }
-            void RenderDrawRect(int x, int y, int w, int h) {
+            void              RenderDrawRect(int x, int y, int w, int h) {
                 SDL_Rect r = { x,y,w,h };
                 SDL_RenderDrawRect(renderer, &r);
             }
-            void DrawInventory(int cols, int rows) {
+            void              DrawInventory(int cols, int rows) {
                 static bool debug = true; 
                 int w = ((float)windowSize.m_w/(float)cols-1);
                 int h = ((float)windowSize.m_h/(float)rows-1);
@@ -152,7 +161,7 @@ namespace Skele_lib {
                 }
                 debug = false; 
             }
-            auto TakeScreenShot() {
+            auto              TakeScreenShot() {
                 auto surface = SDL_GetWindowSurface(window);
                 auto now = std::chrono::system_clock::now();
                 auto h = std::format("{:%H}", now);
